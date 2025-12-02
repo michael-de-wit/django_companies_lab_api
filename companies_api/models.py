4a6936982d8ffc764dd63c05cb9c42edb1d6f009
michael-de-wit <michael.b.dewit@gmail.com>
2025-12-02 13:51:01 -0800

    link to location

--- companies_api/models.py
+++ companies_api/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=64)
     industry = models.CharField(max_length=64)
+    location = models.ForeignKey(
+        'locations_api.Location',
+        on_delete=models.CASCADE,
+        db_column='location_id',
+        to_field='id',
+        null=True,
+        blank=True
+    )
--- companies_api/serializers.py
+++ companies_api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers 
-from .models import Company 
+from .models import Company
+from locations_api.serializers import LocationSerializer
 
-class CompanySerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
+class CompanySerializer(serializers.ModelSerializer):
+    location = LocationSerializer(read_only=True)
+    location_id = serializers.IntegerField(allow_null=True, required=False)
+    
     class Meta:
-        model = Company # tell django which model to use
-        fields = ('id', 'name', 'industry',) # tell django which fields to include
+        model = Company
+        fields = ('id', 'name', 'industry', 'location_id', 'location')
+
+# class CompanySerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
+#     class Meta:
+#         model = Company # tell django which model to use
+#         fields = ('id', 'name', 'industry') # tell django which fields to include
